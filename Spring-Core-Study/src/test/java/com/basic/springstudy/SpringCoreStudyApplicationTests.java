048d3f00d8ceb08798cfe958f9f9a41da41737ab
kimdoha <kkhhjj3393@gmail.com>
2023-08-22 17:08:32 +0900

    feat: Initial commit

---
+++ Spring-Core-Study/src/test/java/com/basic/springstudy/SpringCoreStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
