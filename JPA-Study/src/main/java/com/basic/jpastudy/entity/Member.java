a6132f4b879b8c2d6cfffca8c91bed26aa55c91f
kimdoha <kkhhjj3393@gmail.com>
2023-08-29 16:51:36 +0900

    feat: 단방향 연관관계 (#7)

--- JPA-Study/src/main/java/com/basic/jpastudy/entity/Member.java
+++ JPA-Study/src/main/java/com/basic/jpastudy/entity/Member.java
@@ -1,20 +1,17 @@
 package com.basic.jpastudy.entity;
 
-import com.basic.jpastudy.entity.enums.RoleType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import java.time.ZonedDateTime;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -34,37 +31,17 @@ public class Member {
   @Column(nullable = false)
   private String name;
 
-  private String city;
-  private String street;
-  @Column(name = "zipcode")
-  private String zipCode;
-
-  @Enumerated(value = EnumType.STRING)
-  private RoleType roleType;
-
-  @OneToMany
-  private List<Order> orderList;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "team_id")
+  private Team team;
 
   private ZonedDateTime createdDate;
   private ZonedDateTime lastModifiedDate;
 
-  public Member update(
-      String name,
-      String city,
-      String street,
-      String zipCode
-  ) {
-    this.name = name;
-    this.city = city;
-    this.street = street;
-    this.zipCode = zipCode;
-
-    return this;
-  }
-
-  public static Member of(String name) {
+  public static Member of(String name, Team team) {
     return Member.builder()
         .name(name)
+        .team(team)
         .build();
   }
 
--- JPA-Study/src/main/java/com/basic/jpastudy/entity/Team.java
+++ JPA-Study/src/main/java/com/basic/jpastudy/entity/Team.java
@@ -4,9 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import java.util.List;
+import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -14,16 +15,25 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "items")
+@Table(name = "teams")
 @Entity
-public class Item {
+public class Team {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
-  private Integer price;
-  private Integer stockQuality;
 
-  @OneToMany
-  private List<OrderItem> orderItemList;
+  private ZonedDateTime createdDate;
+  private ZonedDateTime lastModifiedDate;
+
+  @PrePersist
+  void onPrePersist() {
+    this.createdDate = ZonedDateTime.now();
+    this.lastModifiedDate = ZonedDateTime.now();
+  }
+
+  @PreUpdate
+  void onPreUpdate() {
+    this.lastModifiedDate = ZonedDateTime.now();
+  }
 }
