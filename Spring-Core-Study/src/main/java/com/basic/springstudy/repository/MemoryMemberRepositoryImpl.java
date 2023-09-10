66938209138491e176a927d553fbd59d2221b87f
kimdoha <kkhhjj3393@gmail.com>
2023-09-10 16:47:22 +0900

    feat: 컨테이너에 등록된 모든 빈 조회

---
+++ Spring-Core-Study/src/main/java/com/basic/springstudy/repository/MemoryMemberRepositoryImpl.java
@@ -0,0 +1,20 @@
+package com.basic.springstudy.repository;
+
+import com.basic.springstudy.entity.Member;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepositoryImpl implements MemoryMemberRepository {
+
+  private static Map<Long, Member> store = new HashMap<>();
+
+  @Override
+  public void save(Member member) {
+    store.put(member.getId(), member);
+  }
+
+  @Override
+  public Member findById(Long memberId) {
+    return store.get(memberId);
+  }
+}
