d6183124b3844d09bd724d8f9a48f5327052ef2f
kimdoha <kkhhjj3393@gmail.com>
2023-09-11 01:42:01 +0900

    feat: 스프링 컨테이너를 사용하는 테스트 코드

--- Spring-Core-Study/src/test/java/com/basic/springstudy/singleton/SingleTonTest.java
+++ Spring-Core-Study/src/test/java/com/basic/springstudy/singleton/SingleTonTest.java
@@ -5,6 +5,8 @@ import com.basic.springstudy.service.member.MemberService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingleTonTest {
 
@@ -31,5 +33,20 @@ public class SingleTonTest {
     SingletonService singletonService2 = SingletonService.getInstance();
 
     Assertions.assertTrue(singletonService1 == singletonService2);
+  }
+
+  @Test
+  @DisplayName("스프링 컨테이너와 싱글톤")
+  void springContainer() {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    //1. 조회: 호출할 때 마다 같은 객체를 반환
+    MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+    //2. 조회: 호출할 때 마다 같은 객체를 반환
+    MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+    //참조값이 같은 것을 확인
+    Assertions.assertTrue(memberService1 == memberService2);
   }
 }
